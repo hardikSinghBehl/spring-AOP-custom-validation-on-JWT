13dc7183c0dfe4500084e54599e9b3d897a1aa0d
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-19 14:07:16 +0530

    adding country data on bootstrap

---
+++ src/main/java/com/hardik/crackerjack/bootstrap/MasterCountryDataBootstrap.java
@@ -0,0 +1,32 @@
+package com.hardik.crackerjack.bootstrap;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.hardik.crackerjack.entity.MasterCountry;
+import com.hardik.crackerjack.repository.MasterCountryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class MasterCountryDataBootstrap {
+
+	private final MasterCountryRepository masterCountryRepository;
+	private final List<String> countryList = List.of("India", "Netherlands", "Australia", "Netherlands");
+
+	@PostConstruct
+	public void masterCountryDataBootstrapHandler() {
+		for (int i = 0; i < countryList.size() - 1; i++) {
+			final var country = new MasterCountry();
+			System.out.println(i);
+			country.setId(i);
+			country.setName(countryList.get(i));
+			masterCountryRepository.save(country);
+		}
+	}
+
+}
