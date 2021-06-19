efc9aae8ef8ec9564f78db757131d1c560ad0a0a
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-19 14:16:01 +0530

    added Open-API config classes

---
+++ src/main/java/com/hardik/crackerjack/configuration/properties/OpenApiConfigurationProperties.java
@@ -0,0 +1,37 @@
+package com.hardik.crackerjack.configuration.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+@Data
+@ConfigurationProperties(prefix = "com.hardik.crackerjack")
+public class OpenApiConfigurationProperties {
+
+	private Swagger swagger = new Swagger();
+
+	@Data
+	public class Swagger {
+		private String title;
+		private String description;
+		private String apiVersion;
+
+		private Contact contact = new Contact();
+		private Security security = new Security();
+
+		@Data
+		public class Contact {
+			private String email;
+			private String name;
+			private String url;
+		}
+
+		@Data
+		public class Security {
+			private String name;
+			private String scheme;
+			private String bearerFormat;
+		}
+	}
+
+}
