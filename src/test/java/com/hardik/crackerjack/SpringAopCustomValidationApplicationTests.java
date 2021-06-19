58436e0be47e3538c74a608d99f22a95c6d85dca
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-19 13:14:31 +0530

    init project commit

---
+++ src/test/java/com/hardik/crackerjack/SpringAopCustomValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardik.crackerjack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopCustomValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
