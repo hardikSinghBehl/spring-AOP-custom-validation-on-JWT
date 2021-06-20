f64232327664252674f12a5831038005783dd59c
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-20 14:14:21 +0530

    update

--- src/main/java/com/hardik/crackerjack/controller/CountryController.java
+++ src/main/java/com/hardik/crackerjack/controller/CountryController.java
@@ -3,13 +3,16 @@ package com.hardik.crackerjack.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hardik.crackerjack.dto.CountryDto;
 import com.hardik.crackerjack.repository.MasterCountryRepository;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -19,6 +22,8 @@ public class CountryController {
 	private final MasterCountryRepository masterCountryRepository;
 
 	@GetMapping("/countries")
+	@ResponseStatus(value = HttpStatus.OK)
+	@Operation(summary = "Returns list of countries in the system")
 	public ResponseEntity<List<CountryDto>> countryListRetreivalHandler() {
 		return ResponseEntity.ok(masterCountryRepository.findAll().parallelStream()
 				.map(country -> CountryDto.builder().id(country.getId()).name(country.getName()).build())
